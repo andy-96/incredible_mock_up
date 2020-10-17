32481e95fb4c5504ff22453a97605923dacd8780
Andy Chen <mail@andychen.de>
2020-10-17 17:34:30 +0200

    add trackers

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,28 @@ export default {
   name: 'App',
   methods: {
     async clickOnPreorder() {
-      await db.collection('preorder-tracker').add({})
-      .then(() => alert('This will be soon available!'))
-      .catch(err => console.error(err))
+      await db.collection('tracker').doc('preorder-tracker').get()
+        .then(async item => {
+          const newCount = item.data().count + 1
+          await db.collection('tracker').doc('preorder-tracker').update({
+            count: newCount
+          })
+          .catch(err => console.error(err))
+        })
+        .then(() => alert('Sorry, aktuell ist es nicht mehr verfügbar. Versuche es später nochmal!'))
+        .catch(err => console.error(err))
     }
+  },
+  async mounted() {
+    await db.collection('tracker').doc('page-tracker').get()
+      .then(async item => {
+        const newCount = item.data().count + 1
+        await db.collection('tracker').doc('page-tracker').update({
+          count: newCount
+        })
+        .catch(err => console.error(err))
+      })
+      .catch(err => console.error(err))
   }
 }
 </script>
