9ca91300ca68a4ed99ff1e188a7abc6113581ea6
Andy Chen <mail@andychen.de>
2020-10-23 17:17:31 +0200

    add motivation and incredible box sections

---
+++ src/Components/Diagram.vue
@@ -0,0 +1,77 @@
+<template lang="pug">
+.diagram
+  .diagram__container
+    .diagram__column(
+      :style="{ transform: 'translate(-40px)' }"
+    )
+      h4.diagram__indicator {{ insectValueDescription }}
+      .diagram__bar.dark(
+        :style="{ height: insectValue * 0.6 + 'vh' }"
+      )
+      .diagram__icon
+    .diagram__column(
+      :style="{ transform: 'translate(40px)' }"
+    )
+      h4.diagram__indicator {{ meatValueDescription }}
+      .diagram__bar.bright(
+        :style="{ height: meatValue * 0.6  + 'vh' }"
+      )
+      .diagram__icon
+  h4.diagram__description {{ description }}
+
+</template>
+
+<script>
+export default {
+  name: "Diagram",
+  props: {
+    insectValue: Number,
+    meatValue: Number,
+    insectValueDescription: String,
+    meatValueDescription: String,
+    description: String
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import ../sass/mystyles
+
+.dark
+  background-color: $primary-dark
+
+.bright
+  background-color: $primary
+
+h4
+  font-size: 1.5rem
+  color: $primary-dark
+
+.diagram
+  width: 250px
+  height: 70vh
+  position: relative
+
+  .diagram__container
+    display: flex
+    justify-content: space-around
+    
+    .diagram__column
+      position: absolute
+      bottom: 50px
+
+      .diagram__indicator
+        text-align: center
+        
+      .diagram__bar
+        width: 3rem
+        border-radius: 0.5rem
+
+  .diagram__description
+    position: absolute
+    bottom: 0
+    margin: auto
+    left: 0
+    right: 0
+    text-align: center
+</style>
--- src/Views/LandingPage.vue
+++ src/Views/LandingPage.vue
@@ -4,7 +4,7 @@ div
     .container
       .header__text
         h1.header__title Wie wäre es mal mit was neuem?
-        h2.header__subtitle Probiere jetzt die Zukunft
+        h3 Probiere jetzt die Zukunft
         .header__buttons
           b-button(rounded).button.header__button-order Jetzt bestellen
           b-button(rounded).button.header__button-more Mehr erfahren
@@ -12,13 +12,33 @@ div
       img.header__second-box(src="../images/incredible-box.png")
       img.header__third-box(src="../images/incredible-box.png")
   .navbar-spacer#motivation
-  .section.custom-section
-    .container
-      h1.title Motivation
-  .navbar-spacer#the-incredible-box
-  .section.custom-section
-    .container
-      h1.title Produkt
+  .section.custom-section.motivation
+    .container.motivation__container
+      .text-block
+        h3 Gut für dich + gut für die Umwelt
+        p Während sich die Studien über krebsauslösende Stoffe in Rindfleisch und Geflügel häufen, gibt es eine gesunde und bereits weltweit verbreitete Alternative: Insekten. Besonders durch ihren hohen Anteil an ungesättigten Fettsäuren, Vitaminen und Mineralstoffen werden Insekten bei Kraft- und Leistungssportlern seit einigen Jahren immer beliebter. Die Zahlen sprechen für sich:
+      .motivation__diagrams
+        diagram(
+          v-for="data in diagramData"
+          :insectValue="data.insectValue"
+          :meatValue="data.meatValue"
+          :insectValueDescription="data.insectValueDescription"
+          :meatValueDescription="data.meatValueDescription"
+          :description="data.description"
+        )
+  .navbar-spacer#the-incredible-box.the-incredible-box
+  .section.custom-section.the-incredible-box
+    .container.the-incredible-box__container
+      img.the-incredible-box__first-box(src="../images/incredible-box.png")
+      img.the-incredible-box__second-box(src="../images/incredible-box.png")
+      .text-block
+        h3 The Incredible Box
+        p Wo soll man eigentlich anfangen, wenn man in die vielseitige kulinarische Welt der Insekten eintauchen möchte? Unsere von Hand zusammengestellte INCREDIBLE BOX enthält dieleckersten und erfolgreichsten Produkte – vom hochwertigen Proteinpulver für den Muskelaufbau bis hin zu köstlichen Snacks für Zwischendurch. Das perfekte Geschenk für Freunde oder für dich selbst!
+  .navbar-spacer
+  .section.custom-section.our-values
+    .container.our-values__container
+      h2.our-values__title Das ist uns 
+      h2.our-values__title besonders wichtig
   .navbar-spacer#preorder
   .section.custom-section
     .container
@@ -38,6 +58,7 @@ div
 
 <script>
 import { db } from '../db'
+import Diagram from '../Components/Diagram'
 import emailjs from 'emailjs-com'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -46,8 +67,34 @@ const { VUE_APP_emailjs_serviceID, VUE_APP_emailjs_templateID, VUE_APP_emailjs_u
 
 export default {
   name: "LandingPage",
+  components: {
+    diagram: Diagram
+  },
   data: () => ({
-    preorderEmail: ''
+    preorderEmail: '',
+    diagramData: [
+      {
+        insectValue: 100,
+        meatValue: 33,
+        insectValueDescription: '62%',
+        meatValueDescription: '23%',
+        description: 'Protein pro 100 Gramm'
+      },
+      {
+        insectValue: 10,
+        meatValue: 90,
+        insectValueDescription: '2,7 KG',
+        meatValueDescription: '23 KG',
+        description: 'CO2 Äquivalente pro KG'
+      },
+      {
+        insectValue: 100,
+        meatValue: 15,
+        insectValueDescription: '38 SQM',
+        meatValueDescription: '3,6 SQM',
+        description: 'Landnutzung pro Kilogramm'
+      },
+    ]
   }),
   methods: {
     async clickOnPreorder() {
@@ -76,12 +123,28 @@ export default {
 <style lang="sass" scoped>
 @import ../sass/mystyles
 
+h2
+  font-size: 4rem
+  color: $primary-dark
+
+h3
+  font-size: 2rem
+  color: $primary-dark
+  margin-bottom: 1rem
+
+p
+  font-size: 0.9rem
+  font-family: $text-font
+  line-height: 200%
+  color: $gray-text
+
 .custom-section:first-child
   height: 100vh
   margin-top: -84px
 
 .custom-section
   height: 100vh
+  padding-top: 10vh
 
 .navbar-spacer
   height: 56px
@@ -105,16 +168,10 @@ export default {
     width: 37vw
     z-index: 1
     position: absolute
-    top: 50%
-    transform: translateY(-70%)
+    top: 30%
 
     .header__title
       font-size: 6rem
-      color: $primary-dark
-      margin-bottom: 1rem
-
-    .header__subtitle
-      font-size: 2rem
       color: $primary-dark
       margin-bottom: 1rem
 
@@ -138,7 +195,7 @@ export default {
     position: absolute
     width: 50vw
     transform: rotate(20deg)
-    top: 5vh
+    top: 10vh
     right: 0
   
   .header__third-box
@@ -146,7 +203,49 @@ export default {
     width: 45vw
     transform: rotate(-12deg)
     right: 20vw
-    top: 45vh
+    top: 55vh
 
+.motivation
+  .motivation__container
+    display: flex
+    position: relative
+    
+    .text-block
+      padding-top: 5vh
+      flex: 1
+      width: 30rem
+    
+    .motivation__diagrams
+      flex: 2
+      float: right
+      display: flex
+
+.the-incredible-box
+  background-color: $gray-bg
+  .the-incredible-box__container
+
+    .the-incredible-box__first-box
+      position: absolute
+      width: 50vw
+      transform: rotate(14deg)
+      top: -10vh
+      left: -15vw
+
+    .the-incredible-box__second-box
+      position: absolute
+      width: 45vw
+      transform: rotate(-12deg)
+      top: 40vh
+      left: 0
+
+    .text-block
+      width: 30rem
+      float: right
+
+.our-values
+  .our-values__container
+    .our-values__title
+      text-align: center
+      
 
 </style>
