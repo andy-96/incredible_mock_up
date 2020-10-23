514987366e4e526f7a8634fb7783cda5c818b2e3
Andy Chen <mail@andychen.de>
2020-10-23 18:34:45 +0200

    Revert "refactor code"
    This reverts commit 4b991d1bd63fa53aa59d4c6353287f0b8e9d72d1.

--- src/Views/LandingPage.vue
+++ src/Views/LandingPage.vue
@@ -1,25 +1,53 @@
 <template lang="pug">
 div
-  banner
+  .section.custom-section#header
+    .container
+      .header__text
+        h1.header__title Wie wäre es mal mit was neuem?
+        h3 Probiere jetzt die Zukunft
+        .header__buttons
+          b-button(rounded).button.header__button-order Jetzt bestellen
+          b-button(rounded).button.header__button-more Mehr erfahren
+      img.header__first-box(src="../images/incredible-box.png")
+      img.header__second-box(src="../images/incredible-box.png")
+      img.header__third-box(src="../images/incredible-box.png")
   .navbar-spacer#motivation
-  motivation
+  .section.custom-section.motivation
+    .container.motivation__container
+      .text-block
+        h3 Gut für dich + gut für die Umwelt
+        p Während sich die Studien über krebsauslösende Stoffe in Rindfleisch und Geflügel häufen, gibt es eine gesunde und bereits weltweit verbreitete Alternative: Insekten. Besonders durch ihren hohen Anteil an ungesättigten Fettsäuren, Vitaminen und Mineralstoffen werden Insekten bei Kraft- und Leistungssportlern seit einigen Jahren immer beliebter. Die Zahlen sprechen für sich:
+      .motivation__diagrams
+        diagram(
+          v-for="data in diagramData"
+          :insectValue="data.insectValue"
+          :meatValue="data.meatValue"
+          :insectValueDescription="data.insectValueDescription"
+          :meatValueDescription="data.meatValueDescription"
+          :description="data.description"
+        )
   .navbar-spacer#the-incredible-box.the-incredible-box
-  the-incredible-box
+  .section.custom-section.the-incredible-box
+    .container.the-incredible-box__container
+      img.the-incredible-box__first-box(src="../images/incredible-box.png")
+      img.the-incredible-box__second-box(src="../images/incredible-box.png")
+      .text-block
+        h3 The Incredible Box
+        p Wo soll man eigentlich anfangen, wenn man in die vielseitige kulinarische Welt der Insekten eintauchen möchte? Unsere von Hand zusammengestellte INCREDIBLE BOX enthält dieleckersten und erfolgreichsten Produkte – vom hochwertigen Proteinpulver für den Muskelaufbau bis hin zu köstlichen Snacks für Zwischendurch. Das perfekte Geschenk für Freunde oder für dich selbst!
   .navbar-spacer
   .section.custom-section.our-values
     .container.our-values__container
       h2.our-values__title Das ist uns 
-      h2.our-values__title
-        span.underline besonders
-        |  wichtig
-  .navbar-spacer#preorder.preorder
-  .section.preorder__section.preorder
+      h2.our-values__title besonders wichtig
+  .navbar-spacer#preorder
+  .section.custom-section
     .container
-      h3 Worauf wartest du noch?
-      p Du möchtest eine INCREDIBLE BOX bestellen? Für nur 39,99 EUR erhältst du ein spannendes Überraschungspaket mit einer Auswahl an leckeren Insekten-Snacks von verschiedenen Herstellern. 
-      b-input(placeholder="Name" v-model="preorderName" rounded).preorder__input
-      b-input(placeholder="E-Mail" v-model="preorderEmail" rounded).preorder__input
-      b-button.button.preorder__button(@click="clickOnPreorder" rounded) Jetzt bestellen
+      h1.title Preorder
+      b-field(
+        label='Email'
+      )
+        b-input(placeholder="max.mustermann@email.de" v-model="preorderEmail")
+      button#preorder-button(@click="clickOnPreorder") Preorder Now!
   .navbar-spacer#contact
   .section.custom-section
     .container
@@ -30,9 +58,7 @@ div
 
 <script>
 import { db } from '../db'
-import Banner from '../Components/Banner'
-import Motivation from '../Components/Motivation'
-import TheIncredibleBox from '../Components/TheIncredibleBox'
+import Diagram from '../Components/Diagram'
 import emailjs from 'emailjs-com'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -42,13 +68,33 @@ const { VUE_APP_emailjs_serviceID, VUE_APP_emailjs_templateID, VUE_APP_emailjs_u
 export default {
   name: "LandingPage",
   components: {
-    banner: Banner,
-    motivation: Motivation,
-    'the-incredible-box': TheIncredibleBox
+    diagram: Diagram
   },
   data: () => ({
-    preorderName: '',
-    preorderEmail: ''
+    preorderEmail: '',
+    diagramData: [
+      {
+        insectValue: 100,
+        meatValue: 33,
+        insectValueDescription: '62%',
+        meatValueDescription: '23%',
+        description: 'Protein pro 100 Gramm'
+      },
+      {
+        insectValue: 10,
+        meatValue: 90,
+        insectValueDescription: '2,7 KG',
+        meatValueDescription: '23 KG',
+        description: 'CO2 Äquivalente pro KG'
+      },
+      {
+        insectValue: 100,
+        meatValue: 15,
+        insectValueDescription: '38 SQM',
+        meatValueDescription: '3,6 SQM',
+        description: 'Landnutzung pro Kilogramm'
+      },
+    ]
   }),
   methods: {
     async clickOnPreorder() {
@@ -74,13 +120,8 @@ export default {
 }
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
 @import ../sass/mystyles
-
-h1
-  font-size: 6rem
-  color: $primary-dark
-  margin-bottom: 1rem
 
 h2
   font-size: 4rem
@@ -106,10 +147,7 @@ p
   padding-top: 10vh
 
 .navbar-spacer
-  height: 84px
-
-.underline
-  text-decoration: underline $accent
+  height: 56px
 
 .button
   font-size: 0.8rem
@@ -121,25 +159,93 @@ p
   border-style: none
   box-shadow: 0px 3px 6px $shadow-color
 
+#header
+  background-color: $gray-bg
+  display: flex
+  position: relative
+
+  .header__text
+    width: 37vw
+    z-index: 1
+    position: absolute
+    top: 30%
+
+    .header__title
+      font-size: 6rem
+      color: $primary-dark
+      margin-bottom: 1rem
+
+    .header__button-order
+      color: white
+      background-color: $primary-dark
+      margin-right: 1rem
+      
+    .header__button-more
+      color: white
+      background-color: $gray-button
+
+  .header__first-box
+    position: absolute
+    width: 45vw
+    transform: rotate(-9deg)
+    top: -10vh
+    right: -32vw
+
+  .header__second-box
+    position: absolute
+    width: 50vw
+    transform: rotate(20deg)
+    top: 10vh
+    right: 0
+  
+  .header__third-box
+    position: absolute
+    width: 45vw
+    transform: rotate(-12deg)
+    right: 20vw
+    top: 55vh
+
+.motivation
+  .motivation__container
+    display: flex
+    position: relative
+    
+    .text-block
+      padding-top: 5vh
+      flex: 1
+      width: 30rem
+    
+    .motivation__diagrams
+      flex: 2
+      float: right
+      display: flex
+
+.the-incredible-box
+  background-color: $gray-bg
+  .the-incredible-box__container
+
+    .the-incredible-box__first-box
+      position: absolute
+      width: 50vw
+      transform: rotate(14deg)
+      top: -10vh
+      left: -15vw
+
+    .the-incredible-box__second-box
+      position: absolute
+      width: 45vw
+      transform: rotate(-12deg)
+      top: 40vh
+      left: 0
+
+    .text-block
+      width: 30rem
+      float: right
+
 .our-values
   .our-values__container
     .our-values__title
       text-align: center
-
-.preorder__section
-  padding-bottom: 132px
-
-.preorder
-  background-color: $primary-bright
-  margin-left: auto
-  margin-right: auto
-
-  .preorder__button
-    color: white
-    background-color: $primary-dark
-
-  .preorder__input
-    width: 30rem
-
+      
 
 </style>
